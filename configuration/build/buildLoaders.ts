745fae14d680172d42392948833038e90482f3fc
Artem <varivonchikartem@gmail.com>
2024-12-29 12:52:11 -0800

    Add: Enhance the workflow with SCSS by incorporating the mini-css-extract-plugin, modularizing style isolation into separate files

--- configuration/build/buildLoaders.ts
+++ configuration/build/buildLoaders.ts
@@ -1,9 +1,34 @@
-export function buildLoaders() {
+import { BuildOptions } from "./types/configuration";
+
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildLoaders(options: BuildOptions) {
+  const { isDevelopment } = options;
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            namedExport: false,
+            exportLocalsConvention: "as-is",
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDevelopment ? "[path][name]__[local]--[hash:base64:5]" : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader];
+  return [typescriptLoader, cssLoader];
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import "./index.scss";
+
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
